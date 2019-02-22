de5059b82798c5c09e28a22f7fe9dfb964231878
simran2684 <47336181+simran2684@users.noreply.github.com>
2019-02-21 18:53:53 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import sun.audio.*;
 import java.util.Iterator;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -10,11 +11,13 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import java.io.*;
 
 //we are going to use this code: https://www.youtube.com/watch?v=lQEEby394qg as our base project.
 
 public class Main extends Application {
 
+	
     private HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
 
     private ArrayList<Node> platforms = new ArrayList<Node>();
@@ -24,13 +27,9 @@ public class Main extends Application {
     private Pane appRoot = new Pane();
     private Pane gameRoot = new Pane();
     private Pane uiRoot = new Pane();
-
     private Avatar player;
-
     private int levelWidth;
-
     private boolean running = true;
-    
     
     //image files
     Image tile = new Image("tile.png");
@@ -40,10 +39,28 @@ public class Main extends Application {
     Image left_spike_image = new Image("leftspikes.png");
 
 
-    private void initContent() {
-        Rectangle bg = new Rectangle(42*32, 20*32);
 
-        levelWidth = LevelData.LEVEL1[0].length() * 32;
+	//plays background music and sound effects 
+	//refrence https://www.youtube.com/watch?v=3q4f6I5zi2w 
+	//Background music from http://freemusicarchive.org/music/Kevin_MacLeod/Impact/Impact_Prelude_1765
+	//sound effect from http://soundbible.com/1343-Jump.html
+	public static void music(String name){
+		InputStream playMusic;
+		try{
+		playMusic = new FileInputStream(new File(name));
+		AudioStream audio = new AudioStream(playMusic);
+		AudioPlayer.player.start(audio);
+		
+		}
+		catch(Exception e){
+			System.out.println("error");
+			}
+		}
+	
+    private void initContent() {
+        Rectangle bg = new Rectangle(1245, 588);
+
+        levelWidth = LevelData.LEVEL1[0].length() * 30;
         
         //Draw level
         for (int i = 0; i < LevelData.LEVEL1.length; i++) {
@@ -53,27 +70,27 @@ public class Main extends Application {
                     case '0':
                         break;
                     case '1':
-                        Objects platform = new Objects(j*32, i*32, 32, 32, tile);
+                        Objects platform = new Objects(j*30, i*30, 30, 30, tile);
                         platforms.add(platform);
                         gameRoot.getChildren().add(platform);
                         break;
                     case '2':
-                        Objects button = new Objects(j*32, i*32, 32, 32, buttonImage);
+                        Objects button = new Objects(j*30, i*30, 30, 30, buttonImage);
                         buttons.add(button);
                         gameRoot.getChildren().add(button);
                         break;
                     case '3':
-                        Objects up_spike = new Objects(j*32, i*32+11, 32, 32, up_spike_image);
+                        Objects up_spike = new Objects(j*30, i*30+10, 20, 20, up_spike_image);
                         spikes.add(up_spike);
                         gameRoot.getChildren().add(up_spike);
                         break;
                     case '4':
-                        Objects down_spike = new Objects(j*32, i*32, 32, 26, down_spike_image);
+                        Objects down_spike = new Objects(j*30, i*30, 20, 20, down_spike_image);
                         spikes.add(down_spike);
                         gameRoot.getChildren().add(down_spike);
                         break;
                     case '5':
-                        Objects left_spike = new Objects(j*32+10, i*32, 32, 32, left_spike_image);
+                        Objects left_spike = new Objects(j*30 +10, i*30+5, 20, 20, left_spike_image);
                         spikes.add(left_spike);
                         gameRoot.getChildren().add(left_spike);
                         break;
@@ -82,13 +99,13 @@ public class Main extends Application {
         }
         
         //create player
-        player = new Avatar(0, 520, 32, 32);
+        player = new Avatar(0, 520, 30, 30);
         gameRoot.getChildren().add(player);
         player.translateXProperty().addListener((obs, old, newValue) -> {
             int offset = newValue.intValue();
 
             if (offset > 640 && offset < levelWidth - 640) {
-                gameRoot.setLayoutX(-(offset - 640));
+               gameRoot.setLayoutX(-(offset - 640));
             }
         });
 
@@ -98,19 +115,21 @@ public class Main extends Application {
     private void update() {
     	//checks for arrow key input
         if (isPressed(KeyCode.UP) && player.getTranslateY() >= 5) {
+			music("jump.wav");
             player.jumpPlayer();
+			//music("jump.wav");
         }
 
         if (isPressed(KeyCode.LEFT) && player.getTranslateX() >= 5) {
             player.movePlayerX(-4,platforms);
         }
 
-        if (isPressed(KeyCode.RIGHT) && player.getTranslateX() + 32 <= levelWidth - 5) {
+        if (isPressed(KeyCode.RIGHT) && player.getTranslateX() + 30 <= levelWidth - 5) {
             player.movePlayerX(4,platforms);
         }
 
-        if (player.velocity.getY() < 6) {
-            player.velocity = player.velocity.add(0, .5);
+        if (player.velocity.getY() < 10) {
+            player.velocity = player.velocity.add(0, 1);
         }
 
         player.movePlayerY((int)player.velocity.getY(),platforms);
@@ -128,7 +147,6 @@ public class Main extends Application {
             if (!(Boolean)button.getProperties().get("alive")) {
                 it.remove();
                 gameRoot.getChildren().remove(button);
-                System.out.println("Button Pressed.");
                 System.exit(0);
             }
         }
@@ -142,11 +160,13 @@ public class Main extends Application {
         for (Iterator<Node> it = spikes.iterator(); it.hasNext(); ) {
             Node spike = it.next();
             if (!(Boolean)spike.getProperties().get("alive")) {
-                System.out.println("You died!");
+                System.out.println("you died!");
                 gameRoot.getChildren().remove(player);
-                player = new Avatar(0, 17*32, 32, 32);
+                player = new Avatar(0, 520, 30, 30);
                 gameRoot.getChildren().add(player);
 				spike.getProperties().put("alive", true);
+				music("music.wav");
+
             }
         }
     }
@@ -164,7 +184,7 @@ public class Main extends Application {
         Scene scene = new Scene(appRoot);
         scene.setOnKeyPressed(event -> keys.put(event.getCode(), true));
         scene.setOnKeyReleased(event -> keys.put(event.getCode(), false));
-        primaryStage.setTitle("Test Game Demo 1");
+        primaryStage.setTitle("My Nightmare");
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.setResizable(false);
@@ -175,14 +195,13 @@ public class Main extends Application {
                 if (running) {
                     update();
                 }
-
-            
             }
         };
         timer.start();
     }
 
     public static void main(String[] args) {
+		music("music.wav");
         launch(args);
     }
 }
