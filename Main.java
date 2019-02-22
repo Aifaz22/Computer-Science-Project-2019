0f80b376467dcfa130259b3246dda799e7fef6ba
SummerChu <kennyannvo2@hotmail.com>
2019-02-22 13:15:48 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -58,7 +58,7 @@ public class Main extends Application {
                         gameRoot.getChildren().add(platform);
                         break;
                     case '2':
-                        Objects button = new Objects(j*32, i*32, 32, 32, buttonImage);
+                        Objects button = new Objects(j*32, i*32+16, 32, 16, buttonImage);
                         buttons.add(button);
                         gameRoot.getChildren().add(button);
                         break;
