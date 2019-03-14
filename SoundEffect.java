6cdb79d7b7010573128535f9ff4ec2ccbc18914a
SummerChu <kennyannvo2@hotmail.com>
2019-03-14 03:41:26 -0600

    SoundEffect Class Cleanup

--- SoundEffect.java
+++ SoundEffect.java
@@ -1,19 +1,25 @@
 import javafx.scene.media.Media;
 import java.io.*;
 
-public class SoundEffect{
+/**
+ * Class: SoundEffect
+ * Class Use: Holding and Playing a Sound
+ * Methods:
+ * 		Constructor
+ * 		playSound
+ *
+ */
+public class SoundEffect {
 	
+	//Instance Variable
 	String name;
-	public SoundEffect(String name){
+	public SoundEffect(String name) {
 		this.name = name;
 	}
 
-	public Media playSound(){
-		
-	String soundEffect = (name);
-    Media sound = new Media(new File(soundEffect).toURI().toString());
-	
-	return sound;
-		
+	public Media playSound() {
+		String soundEffect = (name);
+		Media sound = new Media(new File(soundEffect).toURI().toString());
+		return sound;
 	}
 }
