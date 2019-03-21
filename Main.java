5dd6de766584ab6dda780967d37df1647d5465fa
Weile0831 <47436216+Weile0831@users.noreply.github.com>
2019-03-20 18:33:14 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -425,6 +425,7 @@ public class Main extends Application {
 				primaryStage.setScene(scene);
 				primaryStage.show();
 				primaryStage.setResizable(false);
+				btnstart.setText("Resume");
 			}
 		});
 		
