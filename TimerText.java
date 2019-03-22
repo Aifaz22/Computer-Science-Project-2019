0fca0528a2a8616d951fab86dc4b167e0a9bc630
Aifaz Dhuka <aifaz.dhuka1@ucalgary.ca>
2019-03-22 17:18:03 -0600

    Initial Commit(extends Timer)

---
+++ TimerText.java
@@ -0,0 +1,27 @@
+public class TimerText extends Timer{
+	public TimerText() {
+		super();
+	}
+	//Getters (String)
+	public String getStringSec() {
+		
+		if (super.getSec() < 10) {
+			return "0" + String.valueOf(super.getSec());
+		}
+		return String.valueOf(super.getSec());
+	}
+	
+	public String getStringMin() {
+		if (super.getMin() < 10) {
+			return "0" + String.valueOf(super.getMin());
+		}
+		return String.valueOf(super.getMin());
+	}
+	
+	public String getStringHour() {
+		if (super.getHour() < 10) {
+			return "0" + String.valueOf(super.getHour());
+		}
+		return String.valueOf(super.getHour());
+	}
+}
